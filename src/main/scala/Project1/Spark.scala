a43f9364648cce801046e004a5b197250e8b95a7
SevinHawkins <TimeBandit2k@gmail.com>
2022-07-10 19:41:48 -0500

    Setting up the Spark session and preparing to create queries

--- src/main/scala/Project1/Spark.scala
+++ src/main/scala/Project1/Spark.scala
@@ -3,18 +3,24 @@ package Project1
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
-object Spark extends App {
-  def sparkMaker(): Unit = {
-    System.setProperty("hadoop.home.dir", "C:\\hadoop")
-    val spark = SparkSession
+object Spark {
+  private var bool : Boolean = false
+  private var spark : SparkSession = _
+
+  def sparkConnect(): Unit = {
+    spark = SparkSession
       .builder
-      .appName("Does this even matter?")
+      .appName("idk im not ur dad")
       .config("spark.master", "local[*]")
       .enableHiveSupport()
       .getOrCreate()
     Logger.getLogger("org").setLevel(Level.ERROR)
-    spark.sparkContext.setLogLevel("ERROR")
-    println("Spark Session created")
-  }
+    println("Connected to Spark")
+
+    spark.SparkContext.setLogLevel("ERROR")
+    val df = spark.read.csv("hdfs://localhost:9000/user/sevin/airports.csv")
+
+
+
 
 }
